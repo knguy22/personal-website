5bb7abc5d5ab61e177634e02c68f6caf17f710f5
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-06-18 16:00:02 -0700

    define data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use chrono::{DateTime, Utc};
+
+enum Status {
+    Reading,
+    Completed,
+    Waiting,
+    Dropped,
+    Hiatus,
+}
+
+enum Chapters {
+    Web(u32),
+    Novel{volume: u32, chapter: u32, part: u32},
+}
+
+struct NovelEntry {
+    country: String,
+    title: String,
+    chapter: Chapters,
+    rating: u32,
+    status: Status,
+    tags: Vec<String>,
+    notes: String,
+    date_modified: DateTime<Utc>,
+}
 
 fn main() -> () {
 
