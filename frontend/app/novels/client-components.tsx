3ecfaca2dffb48e65cc34b52af9ccee18a86dba4
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-06-23 14:59:02 -0700

    refactor

--- frontend/app/novels/client-components.tsx
+++ frontend/app/novels/client-components.tsx
@@ -1,51 +1,6 @@
 'use client';
 
-import React, {useState, useEffect} from 'react';
-import {NovelEntry, NovelEntryCol, compare_chapter, format_chapter, parse_novels} from '../types/novel_types.tsx';
-
-export function ClientPage() {
-  const [isLoading, setLoading] = useState(true);
-  const [novels, setNovels] = useState<NovelEntry[] | null>(null);
-  const [isUp, setIsUp] = useState(false);
-  const [sort_col, setSortCol] = useState<NovelEntryCol>("rating");
-  const [search_content, setSearchContent] = useState<string>("");
-  const [tags, setTags] = useState<String[]>([]);
-
-  // load novels once
-  useEffect(() => {
-    const fetchNovels = async () => {
-      const novels_url = process.env.NEXT_PUBLIC_API_URL + '/novels';
-      try {
-        const response = await fetch(novels_url);
-        const novelsData = await response.json();
-        const convertedNovels = parse_novels(novelsData);
-        setNovels(convertedNovels);
-        setTags(convertedNovels.map((novel) => novel.tags).flat());
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchNovels();
-  }, []);
-
-  if (isLoading) {
-    return (<p>Loading...</p>);
-  }
-  if (!novels) {
-    return (<p>Novels not found...</p>);
-  }
-
-  return (
-    <>
-      <SortByDropdown {...{setIsUp: setIsUp, isUp: isUp, setSortCol: setSortCol}}/>
-      <SearchBar {...{setContent: setSearchContent}}/>
-      <NovelsTable {...{isUp: isUp, novels: novels, sort_col: sort_col, search_content: search_content, tags: tags}}/>
-    </>
-  );
-};
+import {NovelEntry, NovelEntryCol, compare_chapter, format_chapter} from '../types/novel_types.tsx';
 
 type NovelsTableProps = {
   isUp: boolean,
@@ -55,7 +10,7 @@ type NovelsTableProps = {
   tags: String[]
 }
 
-function NovelsTable({isUp, novels, sort_col, search_content, tags}: NovelsTableProps) {
+export function NovelsTable({isUp, novels, sort_col, search_content, tags}: NovelsTableProps) {
   // split the search content into parseable strings
   const cleaned_search_content: String[] = search_content.split(",").filter((content) => content.length > 0);
 
@@ -141,7 +96,7 @@ type NovelEntryRowProps = {
   novel: NovelEntry;
 };
 
-export function NovelEntryRow({ novel}: NovelEntryRowProps) {
+function NovelEntryRow({ novel}: NovelEntryRowProps) {
     return (
         <tr>
         <td>{novel.country}</td>
--- frontend/app/novels/page.tsx
+++ frontend/app/novels/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
-import {ClientPage} from './client-components.tsx';
+'use client';
+import React, {useState, useEffect} from 'react';
+import {NovelEntry, NovelEntryCol, parse_novels} from '../types/novel_types.tsx';
+import {SortByDropdown, SearchBar, NovelsTable} from './client-components.tsx';
 import "./local.css"
 
-export default async function novels() {
+export default function Novels() {
+  const [isLoading, setLoading] = useState(true);
+  const [novels, setNovels] = useState<NovelEntry[] | null>(null);
+  const [isUp, setIsUp] = useState(false);
+  const [sort_col, setSortCol] = useState<NovelEntryCol>("rating");
+  const [search_content, setSearchContent] = useState<string>("");
+  const [tags, setTags] = useState<String[]>([]);
+
+  // load novels once
+  useEffect(() => {
+    const fetchNovels = async () => {
+      const novels_url = process.env.NEXT_PUBLIC_API_URL + '/novels';
+      try {
+        const response = await fetch(novels_url);
+        const novelsData = await response.json();
+        const convertedNovels = parse_novels(novelsData);
+        setNovels(convertedNovels);
+        setTags(convertedNovels.map((novel) => novel.tags).flat());
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNovels();
+  }, []);
+
+  if (isLoading) {
+    return (<p>Loading...</p>);
+  }
+  if (!novels) {
+    return (<p>Novels not found...</p>);
+  }
+
   return (
-    <ClientPage/>
+    <>
+      <SortByDropdown {...{setIsUp: setIsUp, isUp: isUp, setSortCol: setSortCol}}/>
+      <SearchBar {...{setContent: setSearchContent}}/>
+      <NovelsTable {...{isUp: isUp, novels: novels, sort_col: sort_col, search_content: search_content, tags: tags}}/>
+    </>
   );
 };
