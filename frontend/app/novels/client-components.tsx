d605ba8740fb79462e3840eb6ffcd4bc0e4f83e7
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-06-23 13:12:38 -0700

    isolated table components as client components

--- frontend/app/novels/client-components.tsx
+++ frontend/app/novels/client-components.tsx
@@ -1,27 +1,93 @@
 'use client';
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {NovelEntry, create_chapter, format_chapter} from '../types/novel_types.tsx';
+
+export function NovelsTable() {
+  const [novels, setNovels] = useState<NovelEntry[] | null>(null);
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const novels_url = process.env.NEXT_PUBLIC_API_URL + '/api/novels';
+    fetch(novels_url)
+      .then(response => response.json())
+      .then(novels => convert_novels(novels))
+      .then(novels => {
+        setNovels(novels);
+        setLoading(false)
+    })
+  }, []);
+
+  if (isLoading) {
+    return (<p>Loading...</p>);
+  }
+  if (!novels) {
+    return (<p>Novels not found...</p>);
+  }
+
+  return (
+    <>
+    <SortByDropdown {...{isUp: true}}/>
+    <table>
+    <thead>
+        <tr>
+        <th>Country</th>
+        <th>Title</th>
+        <th>Chapter</th>
+        <th>Rating</th>
+        <th>Status</th>
+        <th>Tags</th>
+        <th>Notes</th>
+        <th>Date Modified</th>
+        </tr>
+    </thead>
+    <tbody>
+        {novels.map((novel: NovelEntry, index: number) => (
+        <NovelEntryRow key={index} novel={novel}/>
+        ))}
+    </tbody>
+    </table>
+    </>
+  );
+};
+
+function convert_novels(novels: NovelEntry[]): NovelEntry[] {
+  return novels.map((novel: NovelEntry) => ({
+    country: novel.country,
+    title: novel.title,
+    chapter: create_chapter(novel.chapter),
+    rating: novel.rating,
+    status: novel.status,
+    tags: novel.tags,
+    notes: novel.notes,
+    date_modified: novel.date_modified
+  }));
+}
+
+type NovelEntryRowProps = {
+  novel: NovelEntry;
+};
+
+export function NovelEntryRow({ novel}: NovelEntryRowProps) {
+    return (
+        <tr>
+        <td>{novel.country}</td>
+        <td>{novel.title}</td>
+        <td>{format_chapter(novel.chapter)}</td>
+        <td>{String(novel.rating)}</td>
+        <td>{novel.status}</td>
+        <td>{novel.tags.join(',')}</td>
+        <td>{novel.notes}</td>
+        <td>{String(novel.date_modified)}</td>
+        </tr>
+    )
+}
 
 type SortByDropdownProps = {
   isUp: boolean
 }
 
 export function SortByDropdown({isUp}: SortByDropdownProps) {
-
-  const [sortBy, setSortBy] = useState("country");
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    console.log(sortBy);
-  }
-
-  function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    setSortBy(event.target.value);
-  }
-
-  function handleClick() {
-    setSortBy(sortBy === "country" ? "title" : "country");
-  }
-
   return (
     <>
       <select>
--- frontend/app/novels/page.tsx
+++ frontend/app/novels/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import {SortByDropdown} from './client-components.tsx';
-import { NovelsTable } from './server-components.tsx';
+import {SortByDropdown, NovelsTable} from './client-components.tsx';
 import "./local.css"
 
 export default async function novels() {
   return (
     <>
-      <SortByDropdown {...{isUp: true}}/>
       <NovelsTable/>
     </>
   );
