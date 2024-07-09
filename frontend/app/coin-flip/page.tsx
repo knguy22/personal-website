9383378db6cc2ba5ef52938799fccc63dfe972cb
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-09 13:30:21 -0700

    flipping happens

--- frontend/app/coin-flip/page.tsx
+++ frontend/app/coin-flip/page.tsx
@@ -1,19 +1,42 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import './coin.css'
+import { PanelBottom } from 'lucide-react';
 
 export default function CoinFlip() {
+  const [headSide, setHeadSide] = useState('top');
+  const [tailSide, setTailSide] = useState('bottom');
+
+  function flip() {
+    const side = Math.random() < 0.5 ? 'heads' : 'tails';
+    if (side === 'heads') {
+      setHeadSide('top');
+      setTailSide('bottom');
+    } else {
+      setHeadSide('bottom');
+      setTailSide('top');
+    }
+  }
+
+  useEffect(() => {
+    flip();
+  }, []);
+
+
   return (
     <div className="flex flex-col items-center justify-start h-screen pt-20">
       <div id='coin' className="relative w-60 h-60">
-        <div id='heads' className="bg-red-800">
+        <button id='heads' className={headSide} onClick={flip}>
           <p className='coin-text'>
             Heads
           </p>
-        </div>
-        <div id='tails' className="bg-green-800">
+        </button>
+        <button id='tails' className={tailSide} onClick={flip}>
           <p className='coin-text'>
             Tails
           </p>
-        </div>
+        </button>
       </div>
       <p className="text-4xl flex-col items-center text-center justify-between pt-10">
         Flip the coin!
