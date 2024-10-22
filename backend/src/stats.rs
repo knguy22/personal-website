93680083589be2e53bc875dc283b743f30b44e14
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-10-21 22:44:38 -0700

    add average rating to stats

--- backend/src/stats.rs
+++ backend/src/stats.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 pub struct Stats {
     pub novel_count: u32,
     pub chapter_count: u32,
+    pub average_rating: f32,
 }
 
 pub async fn get_stats(db: &DatabaseConnection) -> Result<Stats, Box<dyn Error>> {
@@ -14,8 +15,17 @@ pub async fn get_stats(db: &DatabaseConnection) -> Result<Stats, Box<dyn Error>>
     let novel_count = novels.len() as u32;
     let chapter_count = novels.iter().map(|novel| novel.chapter.parse().unwrap_or(0)).sum();
 
+    let rating_sum: u32 = novels.iter().map(|novel| novel.rating).sum();
+    let non_zero_ratings = novels.iter().filter(|novel| novel.rating > 0).count();
+    let average_rating = if non_zero_ratings > 0 {
+        rating_sum as f32 / non_zero_ratings as f32
+    } else {
+        0.0
+    };
+
     Ok(Stats {
         novel_count,
         chapter_count,
+        average_rating
     })
 }
--- frontend/app/novels/novels-stats/stats.tsx
+++ frontend/app/novels/novels-stats/stats.tsx
@@ -2,4 +2,5 @@
 export type Stats = {
   novel_count: number,
   chapter_count: number,
+  average_rating: number
 }
