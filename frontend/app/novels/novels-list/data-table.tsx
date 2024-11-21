1f261ffb0b287f07f565e9e324f955788bb2fcf1
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-11-21 10:40:15 -0800

    move filters to separate file

--- frontend/app/novels/novels-list/data-table.tsx
+++ frontend/app/novels/novels-list/data-table.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { useSession } from 'next-auth/react'
-import { FilterFn, Row } from "@tanstack/react-table"
 
 import {
   ColumnDef,
@@ -27,18 +26,9 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
 import { Button } from "../../../components/ui/button"
-import { Input } from "@/components/ui/input"
 
+import { FilterList, filterTags } from './filters'
 import { CreateNovelButton } from './create-novel-button'
 import { DownloadCsvButton } from "./download-csv-button"
 import { DeleteRowButton } from "./delete-row-button"
@@ -217,98 +207,4 @@ function TableOptionsRow({ table, tableData, setTableData }: TableOptionsRowProp
       </div>
     </div>
   );
-}
-
-interface FilterListProp {
-  table: TanstackTable<NovelEntry>,
-}
-
-function FilterList ({ table }: FilterListProp) {
-
-  function findKeyByValue(obj: { [key: string]: keyof NovelEntry }, value: keyof NovelEntry): string | undefined {
-    return Object.entries(obj).find(([key, val]) => val === value)?.[0];
-  }
-
-  const filter_keys: { [key: string]: keyof NovelEntry } = {
-    "Country": "country",
-    "Title": "title",
-    "Tags": "tags",
-    "Rating": "rating",
-    "Status": "status",
-    "Notes": "notes",
-  };
-
-  const [dropDownVal, setDropDownVal] = React.useState<keyof NovelEntry>(filter_keys.Title);
-
-  return (
-    <div className="flex items-center space-x-2">
-      <div className="border border-input bg-background hover:bg-accent hover:text-accent-foreground rounded-md p-2">
-        <DropdownMenu>
-          {/* val to key for dropdown label */}
-          <DropdownMenuTrigger>{findKeyByValue(filter_keys, dropDownVal)}</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>{"Filter by"}</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            {Object.keys(filter_keys).map((key) => (
-              <DropdownMenuItem key={key} onClick={() => setDropDownVal(filter_keys[key])}>{key}</DropdownMenuItem>
-            ))
-            }
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-
-      {/* Filter adjusted based on dropdown */}
-      <Filter
-        table={table}
-        placeholder={"Filter by " + dropDownVal}
-        col_name={dropDownVal}
-      />
-    </div>
-  )
-}
-
-interface FilterProps {
-  table: TanstackTable<NovelEntry>,
-  placeholder: string
-  col_name: keyof NovelEntry
-  class_extra?: string
-}
-
-function Filter({table, placeholder, col_name, class_extra}: FilterProps) {
-  return (
-    <div className="flex items-center space-x-2 pr-2">
-      <Input
-        placeholder={placeholder}
-        value={(table.getColumn(col_name)?.getFilterValue() as string) ?? ""}
-        onChange={(event) => {
-          table.getColumn(col_name)?.setFilterValue(event.target.value);
-          }
-        }
-        className={"max-w-sm" + (class_extra ? " " + class_extra : "")}
-      />
-    </div>
-  )
-}
-
-const filterTags: FilterFn<NovelEntry> = (
-  row: Row<NovelEntry>, 
-  columnId: string, 
-  filterValue: string, 
-  ): boolean => {
-
-  const search_terms = filterValue.toLocaleLowerCase().split(",").map((term) => {
-    return term.trim();
-  });
-  const row_value: string = row.getValue(columnId);
-  const tags: string[] = row_value.toLocaleLowerCase().split(",").map((tag) => {
-    return tag.trim();
-  });
-
-  // only return true if all search terms are in the tags
-  for (const search_term of search_terms) {
-    if (!tags.includes(search_term)) {
-      return false;
-    }
-  }
-  return true;
 }
