4f8a571cee42dc153d841bcd32a3158fb41672f6
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-12-17 21:45:41 -0800

    remove x padding from table columns

--- frontend/app/novels/novels-list/read-only-cell.tsx
+++ frontend/app/novels/novels-list/read-only-cell.tsx
@@ -3,5 +3,5 @@
 import { CellContext } from "@tanstack/react-table";
 
 export function ReadOnlyCell<TData>({ getValue }: CellContext<TData, string>) {
-  return <div className="text-md px-4">{getValue()}</div>
+  return <div className="text-md">{getValue()}</div>
 }
