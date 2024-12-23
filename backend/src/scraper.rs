7f7d189087350e10b0d3d944ddd345f8d8d24187
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-12-22 23:16:23 -0800

    some refactoring

--- backend/src/scraper.rs
+++ backend/src/scraper.rs
@@ -1,30 +1,30 @@
 use anyhow::Result;
 use url::Url;
 use headless_chrome::{Browser, LaunchOptions};
-use std::{thread, time::Duration, path::PathBuf, env};
+use std::{path::PathBuf, thread, time::Duration};
 
-async fn scrape_tags(title: &str) -> Result<Vec<String>> {
+async fn init() -> Result<Browser> {
     let port = Some(1234);
     let chrome_binary = Some(PathBuf::from("./chrome-linux64/chrome"));
-    let url = Url::parse(title)?;
-
-    let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36";
-    let accept_language = Some("en-US,en");
-    let platform = Some("Win32");
-
     let launch_options = LaunchOptions::default_builder()
         .path(chrome_binary)
         .port(port)
         .build()?;
-    let browser = Browser::new(launch_options)?;
+    Browser::new(launch_options)
+}
+
+async fn scrape_tags(browser: &Browser, title: &str) -> Result<Vec<String>> {
+    let url = Url::parse(title)?;
+    let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36";
+    let accept_language = Some("en-US,en");
+    let platform = Some("Win32");
+
     let tab = browser.new_tab()?;
     tab.set_user_agent(user_agent, accept_language, platform)?;
     tab.navigate_to(url.as_str())?;
-
     thread::sleep(Duration::from_secs(3));
 
     let text = tab.get_content()?;
-    println!("{}", text);
 
     todo!()
 }
@@ -34,7 +34,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_scrape_tags() {
-        let res = scrape_tags("https://www.novelupdates.com/series/lord-of-the-mysteries/").await.unwrap();
+        let browser = init().await.unwrap();
+        let res = scrape_tags(&browser, "https://www.novelupdates.com/series/lord-of-the-mysteries/").await.unwrap();
         assert_ne!(res.len(), 0);
     }
 }
