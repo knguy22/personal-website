5b15db91b3032ddada69a8ab967e5945f4d27710
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-12-26 20:19:12 -0800

    add royalroad to novels to fetch

--- backend/src/data_ingestion.rs
+++ backend/src/data_ingestion.rs
@@ -3,10 +3,10 @@ pub mod novelupdates;
 pub mod royalroad;
 pub mod csv;
 
-use crate::db;
+use crate::{db, novel_entry::Provider};
 use crate::novel_entry::NovelEntry;
 
-use anyhow::Result;
+use anyhow::{Error, Result};
 use sea_orm::DatabaseConnection;
 
 use std::{thread, time::Duration};
@@ -17,7 +17,18 @@ pub async fn fetch_novel_tags(conn: &DatabaseConnection) -> Result<()> {
     println!("Attempting to fetch tags for {} novels...", novels.len());
 
     for novel in &novels {
-        let scraped_tags = novelupdates::scrape_genres_and_tags(&novel.title, 5, None).await;
+        // don't do redundant work
+        if !novel.tags.is_empty() {
+            continue;
+        }
+
+        // scrape as required
+        let scraped_tags = match novel.provider {
+            Some(Provider::Novelupdates) => novelupdates::scrape_genres_and_tags(&novel.title, 5, None).await,
+            Some(Provider::Royalroad) => royalroad::scrape_tags(&novel.title, 3).await,
+            None => continue,
+        };
+
         match scraped_tags {
             Ok(new_tags) => {
                 let new_novel = NovelEntry {
@@ -29,13 +40,19 @@ pub async fn fetch_novel_tags(conn: &DatabaseConnection) -> Result<()> {
             },
 
             Err(e) => {
-                println!("Failure: [{}], {}", novel.title, e);
+                println!(
+                r#"Failure:
+                  * Novel: {},  
+                  * Provider: {:?}
+                  * Error: {}"#, 
+                novel.title, novel.provider, e);
                 thread::sleep(Duration::from_secs(15));
             },
         }
     }
 
     db::update_novel_entries(conn, &modified_novels).await?;
+    println!("Finished modifying {} novels", modified_novels.len());
     Ok(())
 }
 
@@ -43,7 +60,11 @@ pub async fn single_fetch_novel_tags(conn: &DatabaseConnection, title: &str, url
     println!("Attempting to fetch tags for [{title}]");
 
     let novel = db::fetch_single_novel(conn, title).await?;
-    let scraped_tags = novelupdates::scrape_genres_and_tags(title, 2, url).await?;
+    let scraped_tags = match novel.provider {
+        Some(Provider::Novelupdates) => novelupdates::scrape_genres_and_tags(title, 2, url).await?,
+        Some(Provider::Royalroad) => royalroad::scrape_tags(title, 3).await?,
+        None => Err(Error::msg(format!("Novel doesn't contain a provider: {}", novel.title)))?
+    };
     let new_novel = vec![NovelEntry {
         tags: scraped_tags,
         ..novel.clone()
