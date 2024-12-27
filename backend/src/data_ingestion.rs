c9fdd914e75ddfd4395b5d628f45aee224373158
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-12-26 22:50:20 -0800

    make fetch all command better

--- backend/src/data_ingestion.rs
+++ backend/src/data_ingestion.rs
@@ -7,26 +7,25 @@ use crate::{db, novel_entry::Provider};
 use crate::novel_entry::NovelEntry;
 
 use anyhow::{Error, Result};
+use itertools::Itertools;
 use sea_orm::DatabaseConnection;
 
 use std::{thread, time::Duration};
 
 pub async fn fetch_novel_tags(conn: &DatabaseConnection) -> Result<()> {
     let novels = db::fetch_novel_entries(conn).await?;
+    let novels_to_fetch = novels.iter()
+        .filter(|novel| novel.tags.is_empty())
+        .filter(|novel| novel.provider.is_some())
+        .collect_vec();
+    println!("Fetching tags for {} novels out of {}...", novels_to_fetch.len(), novels.len());
+
     let mut modified_novels = Vec::new();
-    println!("Attempting to fetch tags for {} novels...", novels.len());
-
-    for novel in &novels {
-        // don't do redundant work
-        if !novel.tags.is_empty() {
-            continue;
-        }
-
+    for novel in novels_to_fetch {
         // scrape as required
-        let scraped_tags = match novel.provider {
-            Some(Provider::Novelupdates) => novelupdates::scrape_genres_and_tags(&novel.title, 5, None).await,
-            Some(Provider::Royalroad) => royalroad::scrape_tags(&novel.title, 3).await,
-            None => continue,
+        let scraped_tags = match novel.provider.as_ref().unwrap() {
+            Provider::Novelupdates => novelupdates::scrape_genres_and_tags(&novel.title, 5, None).await,
+            Provider::Royalroad => royalroad::scrape_tags(&novel.title, 3).await,
         };
 
         match scraped_tags {
@@ -40,11 +39,7 @@ pub async fn fetch_novel_tags(conn: &DatabaseConnection) -> Result<()> {
             },
 
             Err(e) => {
-                println!(
-                r#"Failure:
-                  * Novel: {},  
-                  * Provider: {:?}
-                  * Error: {}"#, 
+                println!("Failure:\n  Novel: {}\n  Provider: {:?}\n  Error: {}", 
                 novel.title, novel.provider, e);
                 thread::sleep(Duration::from_secs(15));
             },
